91e5d63d2df66bea8f446844a288b48c149edeac
Azkka azeem <azkaazeem804@gmail.com>
2026-05-13 12:44:58 +0500

    Update Express.js backend project

---
+++ 1- Express.js/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app =express();
+
+// console.dir(app);
+
+const port = 8080;
+
+app.listen(port , () => {
+    console.log(`app is listening on port ${port}`);
+});
