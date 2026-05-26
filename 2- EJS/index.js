dacf2df36eab7e0ee9e296597d8b6c060eea5285
Azka azeem <azkaazeem804@gmail.com>
2026-05-26 16:07:53 +0500

    ADD MORE IN ejs FOLDER

--- 2- EJS/index.js
+++ 2- EJS/index.js
@@ -16,7 +16,16 @@ app.get('/hello', (req, res) => {
 });
 
 app.get('/rolldice', (req, res) => {
-  res.render('rolldice.ejs');
+    let diceVal = Math.floor(Math.random() * 6) + 1;
+  res.render('rolldice.ejs' , {diceVal});
+});
+
+app.get('/insta/:username', (req, res) => {
+    let {username} = req.params;
+    const instaData = require('./data.json');
+    const data = instaData[username]
+    console.log(data);
+  res.render('insta.ejs'  , {data});
 });
 
 app.listen(PORT, () => {
