ef21f704b548ec9600ca81fc9cd00f756d6056c1
Azka azeem <azkaazeem804@gmail.com>
2026-05-27 19:32:39 +0500

    setup backend and write basic code

--- 3- MISCELLANEOUS/backend/index.js
+++ 3- MISCELLANEOUS/backend/index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8080;
 
 app.use(express.urlencoded ({ extended: true }));
+app.use(express.json());
 
 app.get("/register" , (req , res) => {
     res.send("standard GET response");
